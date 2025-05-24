10b8c9807a32613ea1cf9202657d74037b234778
kolegasior <jkole822@gmail.com>
2025-05-23 21:14:56 -0400

    feature: Collapsible - Enhance docs

--- src/lib/Collapsible/index.stories.ts
+++ src/lib/Collapsible/index.stories.ts
@@ -8,30 +8,52 @@ const meta = {
   title: "Collapsible",
   component: Collapsible,
   tags: ["autodocs"],
+  args: {
+    defaultOpen: false,
+    disabled: false,
+    lazyMount: false,
+    unmountOnExit: false,
+  },
   argTypes: {
-    asChild: { control: "boolean" },
-    className: { control: "text" },
-    open: { table: { disable: true } },
+    asChild: {
+      control: "boolean",
+      description: "Renders the component as a child element.",
+    },
+    className: {
+      control: "text",
+      description: "Class that is applied to top level element.",
+    },
+    defaultOpen: {
+      control: "boolean",
+      description: "The initial open state when rendered.",
+    },
+    disabled: {
+      control: "boolean",
+      description: "Disables the collapsible component.",
+    },
+    lazyMount: {
+      control: "boolean",
+      description:
+        "Whether to mount the content only when the item is expanded for the first time.",
+    },
+    open: {
+      control: "boolean",
+      description: "The controlled open state of the collapsible.",
+    },
+    unmountOnExit: {
+      control: "boolean",
+      description: "Whether to unmount content when the item is collapsed.",
+    },
   },
 } satisfies Meta<typeof Collapsible>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const args = {
-  defaultOpen: false,
-  disabled: false,
-  lazyMount: false,
-  unmountOnExit: false,
-};
-
-export const Basic: Story = {
-  args,
-};
+export const Basic: Story = {};
 
 export const DefaultOpen: Story = {
   args: {
-    ...args,
     defaultOpen: true,
   },
 };
