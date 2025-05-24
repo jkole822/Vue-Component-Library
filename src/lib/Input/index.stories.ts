45754929dcb7aa4db4c4a446f95510300c838004
kolegasior <jkole822@gmail.com>
2025-05-23 21:31:47 -0400

    feature: Input - Enhance docs

--- src/lib/Input/index.stories.ts
+++ src/lib/Input/index.stories.ts
@@ -1,5 +1,6 @@
 // Packages
 import { faker } from "@faker-js/faker";
+import { v4 as uuid } from "uuid";
 
 // Components
 import Input from "./story.vue";
@@ -13,11 +14,24 @@ const meta = {
   title: "Input",
   component: Input,
   tags: ["autodocs"],
+  args: {
+    autoComplete: AutoCompleteEnum.off,
+    description: faker.lorem.sentence(),
+    disabled: false,
+    errorMessage: faker.lorem.sentence(),
+    id: uuid(),
+    name: faker.lorem.word(),
+    readOnly: false,
+    required: true,
+    type: InputTypeEnum.text,
+  },
   argTypes: {
     autoComplete: {
       control: {
         type: "select",
       },
+      description:
+        "Assists users by offering suggestions based on the type of auto-complete.",
       options: [
         AutoCompleteEnum.currentPassword,
         AutoCompleteEnum.email,
@@ -27,12 +41,54 @@ const meta = {
         AutoCompleteEnum.username,
       ],
     },
-    id: { control: "text" },
-    modelValue: { table: { disable: true } },
+    className: {
+      control: "text",
+      description: "Class that is applied to top level element.",
+    },
+    description: {
+      control: "text",
+      description: "The field description.",
+    },
+    disabled: {
+      control: "boolean",
+      description: "Disables the field and its associated input.",
+    },
+    errorMessage: {
+      control: "text",
+      description:
+        "Field error message displayed when `validationState` is `invalid`.",
+    },
+    id: {
+      control: "text",
+      description: "The unique identifier of the machine.",
+    },
+    modelValue: {
+      control: "text",
+      description: "The model value of the field.",
+    },
+    name: {
+      control: "boolean",
+      description:
+        "The `name` attribute of the fields input. Useful for form submission.",
+    },
+    raiseLabel: {
+      control: "boolean",
+      description: "Holds the label at the top of the field.",
+    },
+    readOnly: {
+      control: "boolean",
+      description: "Marks the field as read-only.",
+    },
+    required: {
+      control: "boolean",
+      description: "Marks the field as required.",
+    },
     type: {
       control: {
         type: "select",
       },
+      description:
+        "Informs the input which type of data is expected to be provided by the user.",
       options: [
         InputTypeEnum.date,
         InputTypeEnum.email,
@@ -42,6 +98,7 @@ const meta = {
     },
     validationState: {
       control: "select",
+      description: "Indicates whether field is `valid` or `invalid`",
       options: [ValidationStateEnum.Valid, ValidationStateEnum.Invalid],
     },
   },
@@ -50,30 +107,16 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const args = {
-  autoComplete: AutoCompleteEnum.off,
-  description: faker.lorem.sentence(),
-  errorMessage: faker.lorem.sentence(),
-  name: faker.lorem.word(),
-  readOnly: false,
-  required: true,
-  type: InputTypeEnum.text,
-};
-
-export const Basic: Story = {
-  args,
-};
+export const Basic: Story = {};
 
 export const DateField: Story = {
   args: {
-    ...args,
     type: InputTypeEnum.date,
   },
 };
 
 export const PasswordField: Story = {
   args: {
-    ...args,
     autoComplete: AutoCompleteEnum.newPassword,
     type: InputTypeEnum.password,
   },
